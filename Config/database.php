0241023aa988dd52ad83d25950485e63d6ba840d
arczavin <3day@wp.pl>
2018-04-20 20:20:39 +0200

    poprawki lab1

--- Config/database.php
+++ Config/database.php
@@ -1,28 +1,28 @@
 <?php
 
-class database {
-    static $host, $user, $password, $database;
+  class database {
+      static $host, $user, $password, $database;
 
-    function __construct($host='torus.uck.pk.edu.pl', $user='aglina', $password='niemahasla', $database='aglina')
-    {
-        self::$host=$host;
-        self::$user=$user;
-        self::$password=$password;
-        self::$database=$database;
-    }
+      function __construct($host='torus.uck.pk.edu.pl', $user='aglina', $password='niemahasla', $database='aglina') {
+          self::$host=$host;
+          self::$user=$user;
+          self::$password=$password;
+          self::$database=$database;
+      }
 
-    function connect(){
-        $connection = mysqli_connect(self::$host, self::$user, self::$password, self::$database);
-        if($connection){
-            return $connection;
-        }else{
-            return die('Connection error!');
-        }
-    }
+      function connect() {
+          $connection = mysqli_connect(self::$host, self::$user, self::$password, self::$database);
+          if($connection) {
+              return $connection;
+          }
+          else {
+              return die('Connection error!');
+          }
+      }
 
-    function query($q=""){
-        return mysqli_query($this->connect(), $q);
-    }
-}
+      function query($q="") {
+          return mysqli_query($this->connect(), $q);
+      }
+  }
 
 ?>
--- View/lab01.php
+++ View/lab01.php
@@ -4,17 +4,17 @@
         <?php
 
             echo "Hello World!";
-            
+
             $zmienna="tekst";
-            echo "<h2>Przykładowy \n $zmienna!</h2>";
-            echo "To ", "jest ", "przykładowy ".$zmienna."\n";
-            print "Jeszcze jeden".$zmienna."\n";
-            echo print "Jeszcze jeden".$zmienna."\n";
+            echo "<h2>Przykładowy $zmienna!</h2>";
+            echo "To ", "jest ", "przykładowy " . $zmienna . "\n";
+            print "Jeszcze jeden " . $zmienna . "\n";
+            echo print "Jeszcze jeden " . $zmienna . "\n";
 
             $kolor="red";
-            echo "Wybrany kolor: " . $kolor . "<br>";
+            echo " Wybrany kolor: " . $kolor . "<br>";
             echo "Kolor: " . $KOLOR . "<br>";
-            echo "Kolor: " . $koLOR . "<br>";
+            echo "Kolor: " . $koLOR . "<br><br>";
 
             // Komentarz
             # Komentarz
@@ -24,68 +24,81 @@
 
             ECHO "Tekst HTML<br>";
             echo "Tekst HTML<br>";
-            EcHo "Tekst HTML<br>";
+            EcHo "Tekst HTML<br><br>";
 
             $s = "To JEST \"przykładowy\" <b>ciąg 'tekstowy'</b>";
-            echo "\n 1 ". $s ;
-            echo "\n 2 ". addslashes($s);
-            echo "\n 3 ". crc32($s) ;
-            echo "\n 4 ". explode(" ", $s) ;
-            echo "\n 5 ". htmlspecialchars($s) ;
-            echo "\n 6 ". htmlspecialchars_decode (htmlspecialchars($s)) ;
-            echo "\n 7 ". implode (" ", explode(" ", $s)) ;
-            echo "\n 8 ". md5($s) ;
-            echo "\n 9 ". sha1($s) ;
+            echo "\n 1 ". $s . "<br>";
+            echo "\n 2 ". addslashes($s) . "<br>";
+            echo "\n 3 ". crc32($s) . "<br>";
+            echo "\n 4 ". explode(" ", $s) . "<br>";
+            echo "\n 5 ". htmlspecialchars($s) . "<br>";
+            echo "\n 6 ". htmlspecialchars_decode (htmlspecialchars($s)) . "<br>";
+            echo "\n 7 ". implode (" ", explode(" ", $s)) . "<br>";
+            echo "\n 8 ". md5($s) . "<br>";
+            echo "\n 9 ". sha1($s) . "<br>";
             echo "\n";
 
             $x = 5985; var_dump($x);
-            echo "<br>"; $x = -345; var_dump($x);
-            echo "<br>"; $x = 0x8C; 
+            echo "<br>";
+            $x = -345; var_dump($x);
+            echo "<br>";
+            $x = 0x8C; var_dump($x); // wartość heksadecymalna
+            echo "<br>";
+            $x = 047; var_dump($x); // wartość oktalna
+            echo "<br>";
 
-            // wartość heksadecymalna
-            var_dump($x);
-            echo 
-            "<br>"; $x = 047; 
+            $x = 10.365; var_dump($x);
+            echo "<br>";
+            $x = 2.4e3; var_dump($x);
+            echo "<br>";
+            $x = 8E-5; var_dump($x);
+            echo "<br>";
 
-            // wartość oktalna
+            $x="Helloworld!";
+            $x=null;
             var_dump($x);
-
-            $x="Helloworld!"; $x=null;
-            var_dump($x);
+            echo "<br>";
 
             $x=true; $y=false;
             var_dump($x);
+            echo "<br>";
             var_dump($y);
+            echo "<br>";
 
             $array = [
-                "foo" => "bar", 
+                "foo" => "bar",
                 "bar" => "foo",
             ];
 
-            $ary = array("1"=>'One','Two',"3"=>'Three'); $a = '0'; $b = count($ary); 
-                while($a <= $b) { 
-                    $pr = $ary[$a]; 
-                    print "$pr<br>"; 
-                    $a++; 
-                }
-                
+            $ary = array(1=>'One','Two',3=>'Three');
+            $a = '0';
+            $b = count($ary);
+
+            while($a <= $b) {
+              $pr = $ary [$a];
+              print "$pr <br>";
+              $a++;
+            }
+
             class
                 Student {
                     var $surname;
                     var $name;
+
                     function __construct($surname, $name){
                         $this ->surname = $nazwisko;
-                        $this ->name = $name;
+                        $this ->name = $imie;
                     }
-                
+
                     function setSurname($surname){
                         $this->surname=$surname;
                     }
-                
-                    function getName(){
+
+                    function getName($imie){
                         return
-                        $this->name;}
-                } 
+                        $this->name;
+                    }
+                }
         ?>
     </body>
 </html>
